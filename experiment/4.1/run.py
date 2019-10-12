752b997ca63fa47487bd9e7c07eda90fca28ea95
lntk <lengoctuankhang@gmail.com>
2019-10-12 09:50:40 +0700

    Update run.py

--- experiment/4.1/run.py
+++ experiment/4.1/run.py
@@ -19,7 +19,7 @@ video_dataset = VideoSequenceDataset(
     # data_file="experiment/4/toy_train.txt",
     # path_to_data="data/toy",
     data_file='experiment/4.1/CDnet2014_train.txt',
-    path_to_data='/content/drive/My Drive/Background subtraction/dataset',
+    path_to_data='/content/dataset',
     transform=(transforms.Compose([
             Rescale((128, 128)),
             Normalize(),
